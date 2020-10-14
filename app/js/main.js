0d4c92c4ec769ed6afdc8d96a955e76cb7f62445
Igor Protsenko <protsxe51@gmail.com>
2020-10-14 18:17:16 +0300

    add parallax

--- app/js/main.js
+++ app/js/main.js
@@ -5,30 +5,85 @@ $(function () {
         $('#overlay').toggleClass('open');
     });
 
-    document.getElementById('feedback-form').addEventListener('submit', function (evt) {
-        var http = new XMLHttpRequest(), f = this;
-        evt.preventDefault();
-        http.open("POST", "contacts.php", true);
-        http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        http.send("nameFF=" + f.nameFF.value + "&contactFF=" + f.contactFF.value + "&messageFF=" + f.messageFF.value);
-        http.onreadystatechange = function () {
-            if (http.readyState == 4 && http.status == 200) {
-                alert(http.responseText + ', Ваше сообщение получено.\nНаши специалисты ответят Вам в течении 2-х дней.\nБлагодарим за интерес к нашей фирме!');
-                f.messageFF.removeAttribute('value'); // очистить поле сообщения (две строки)
-                f.messageFF.value = '';
-            }
-        }
-        http.onerror = function () {
-            alert('Извините, данные не были переданы');
-        }
-    }, false);
+    // document.getElementById('feedback-form').addEventListener('submit', function (evt) {
+    //     var http = new XMLHttpRequest(), f = this;
+    //     evt.preventDefault();
+    //     http.open("POST", "contacts.php", true);
+    //     http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    //     http.send("nameFF=" + f.nameFF.value + "&contactFF=" + f.contactFF.value + "&messageFF=" + f.messageFF.value);
+    //     http.onreadystatechange = function () {
+    //         if (http.readyState == 4 && http.status == 200) {
+    //             alert(http.responseText + ', Ваше сообщение получено.\nНаши специалисты ответят Вам в течении 2-х дней.\nБлагодарим за интерес к нашей фирме!');
+    //             f.messageFF.removeAttribute('value'); // очистить поле сообщения (две строки)
+    //             f.messageFF.value = '';
+    //         }
+    //     }
+    //     http.onerror = function () {
+    //         alert('Извините, данные не были переданы');
+    //     }
+    // }, false);
 
 
     $('.dragon-slider').slick({
         infinite: true,
+        arrows: false,
+        autoplay: true,
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 1,
+        autoplaySpeed: 1000,
+        speed: 1000,
+    });
+
+
+    $('.app-slider').slick({
+        infinite: true,
         fade: true,
         arrows: false,
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 3000,
+        speed: 1000,
     });
+
+
+    // $.fn.isOnScreen = function () {
+    //     var win = $(window);
+    //     var viewport = {
+    //         top: win.scrollTop(),
+    //         left: win.scrollLeft()
+    //     };
+    //     viewport.right = viewport.left + win.width();
+    //     viewport.bottom = viewport.top + win.height();
+    //     var bounds = this.offset();
+    //     bounds.right = bounds.left + this.outerWidth();
+    //     bounds.bottom = bounds.top + this.outerHeight();
+    //     return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+    // };
+
+    // $('.about__hero')
+    // console.log($('.about__hero'));
+
+    // $(window).scroll(function () {
+    //     if ($('.about__hero-position--1').isOnScreen()) {
+    //         $('.about__hero-position--1, .about__hero-position--2 ').addClass('animate__animated animate__fadeInDown')
+    //     } else {
+    //         $('.about__hero-position--1, .about__hero-position--2 ').removeClass('animate__animated animate__fadeInDown')
+    //     }
+    //     if ($('.about__hero-position--3').isOnScreen()) {
+    //         $('.about__hero-position--3, .about__hero-position--4 ').addClass('animate__animated animate__fadeInUp')
+    //     } else {
+    //         $('.about__hero-position--3, .about__hero-position--4').removeClass('animate__animated animate__fadeInUp')
+    //     }
+    //     if ($('.about__buble-item').isOnScreen()) {
+    //         $('.about__buble-item img').addClass('animate__pulse animate__infinite animate__slower animate__animated')
+    //     } else {
+    //         $('.about__buble - item img').removeClass('animate__pulse animate__infinite animate__slower animate__animated')
+    //     }
+    // });
+
+
+    var rellax = new Rellax('.rellax', {
+        center: true
+    });
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ gulp.task('css', function () {
   return gulp.src([
     'node_modules/normalize.css/normalize.css',
     'node_modules/slick-carousel/slick/slick.css',
-    'node_modules/animate.css/animate.css',
+    'node_modules/animate.css/animate.css'
   ])
     .pipe(concat('_libs.scss'))
     .pipe(gulp.dest('app/scss'))
@@ -46,7 +46,9 @@ gulp.task('script', function () {
 
 gulp.task('js', function () {
   return gulp.src([
-    'node_modules/slick-carousel/slick/slick.js'
+    'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/rellax/rellax.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js'
   ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
